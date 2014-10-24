e2a86eb5c7f9868fbcfbbb42d9c90cbbc9da8f03
Mike Robinson <mikeprobinsonuk@gmail.com>
2014-10-24 11:57:43 +0100

    Change RETRO_PIXEL_FORMAT_* to static
    It's convenient for these to have defined memory addresses so
    that pointers to them can be passed directly to the frontend.

--- src/rust_wrapper/libretro.rs
+++ src/rust_wrapper/libretro.rs
@@ -1728,11 +1728,11 @@ pub struct retro_disk_control_callback
 * 0 bit must be set to 0.
 * This pixel format is default for compatibility concerns only.
 * If a 15/16-bit pixel format is desired, consider using RGB565. */
-pub const RETRO_PIXEL_FORMAT_0RGB1555: c_uint = 0;
+pub static RETRO_PIXEL_FORMAT_0RGB1555: c_uint = 0;
 
 /* XRGB8888, native endian.
 * X bits are ignored. */
-pub const RETRO_PIXEL_FORMAT_XRGB8888: c_uint = 1;
+pub static RETRO_PIXEL_FORMAT_XRGB8888: c_uint = 1;
 
 /* RGB565, native endian.
 * This pixel format is the recommended format to use if a 15/16-bit
@@ -1740,7 +1740,7 @@ pub const RETRO_PIXEL_FORMAT_XRGB8888: c_uint = 1;
 * available on a wide range of low-power devices.
 *
 * It is also natively supported in APIs like OpenGL ES. */
-pub const RETRO_PIXEL_FORMAT_RGB565: c_uint = 2;
+pub static RETRO_PIXEL_FORMAT_RGB565: c_uint = 2;
 
 #[allow(dead_code)]
 #[repr(C)]
